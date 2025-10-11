8c23ceaa1a3992c5742facfed668239f86ce18b2
SvS-NickM <svs.nickm@outlook.com>
2025-10-11 22:42:22 +0300

    chore: errors tests

--- packages/scope-di/src/internals/scope/default-di-scope.errors.test.ts
+++ packages/scope-di/src/internals/scope/default-di-scope.errors.test.ts
@@ -1,4 +1,12 @@
-import { describe, it } from "@jest/globals";
+import { describe, expect, it } from "@jest/globals";
+import { DependencyNotRegisteredError } from "../../errors/dependency-not-registered-error";
+import { UnknownDependencyTypeError } from "../../errors/unknown-dependency-type-error";
+import type { AllowedDependencyKey } from "../../types/allowed-dependency-key";
+import type { DependencyDescriptor } from "../../types/dependency-descriptor";
+import { DependencyDescriptorType } from "../../types/dependency-descriptor-type";
+import { DependencyLifetime } from "../../types/dependency-lifetime";
+import { DefaultDiScope } from "./default-di-scope";
+import { UnknownDependencyLifetimeError } from "../../errors/unknown-dependency-lifetime-error";
 
 describe
 (
@@ -6,11 +14,86 @@ describe
     () =>
     {
         /*
-         • Unregistered single key: resolve("X") throws DependencyNotRegisteredError.
-         • Empty collection key: resolve(collectionKey("X")) throws DependencyNotRegisteredError.
-         • Unknown descriptor type: a descriptor with an invalid type throws UnknownDependencyTypeError.
          • Unknown lifetime: a descriptor with an invalid lifetime throws UnknownDependencyLifetimeError.
         */
-       it("dummy", () => {});
+        it
+        (
+            "Unregistered single key: resolve() of unregistered key throws DependencyNotRegisteredError", 
+            () => 
+            {
+                const scope = new DefaultDiScope(new Map());
+
+                expect(() => scope.resolve("key" as never)).toThrow(DependencyNotRegisteredError);
+            }
+        );
+
+        it
+        (
+            "Unregistered collection key: resolve() of unregistered collection key throws DependencyNotRegisteredError", 
+            () => 
+            {
+                const scope = new DefaultDiScope(new Map());
+
+                expect(() => scope.resolve(["key"] as never)).toThrow(DependencyNotRegisteredError);
+            }
+        );
+
+        it
+        (
+            "Unknown descriptor type: resolving dependency with a descriptor of an invalid type throws UnknownDependencyTypeError", 
+            () => 
+            {
+                const key = "Key";
+
+                class Dependency {}
+
+                const descriptors = new Map<AllowedDependencyKey, DependencyDescriptor[]>()
+                    .set
+                    (
+                        key,
+                        [
+                            {
+                                key: key,
+                                type: -1 as DependencyDescriptorType,
+                                lifetime: DependencyLifetime.Singleton,
+                                constructor: Dependency
+                            } as DependencyDescriptor
+                        ]
+                    );
+
+                const scope = new DefaultDiScope(descriptors);
+
+                expect(() => scope.resolve(key as never)).toThrow(UnknownDependencyTypeError);
+            }
+        );
+
+        it
+        (
+            "Unknown lifetime: resolving dependency with a descriptor of an invalid lifetime throws UnknownDependencyLifetimeError", 
+            () => 
+            {
+                const key = "Key";
+
+                class Dependency {}
+
+                const descriptors = new Map<AllowedDependencyKey, DependencyDescriptor[]>()
+                    .set
+                    (
+                        key,
+                        [
+                            {
+                                key: key,
+                                type: DependencyDescriptorType.Class,
+                                lifetime: -1 as DependencyLifetime,
+                                constructor: Dependency
+                            } as DependencyDescriptor
+                        ]
+                    );
+
+                const scope = new DefaultDiScope(descriptors);
+
+                expect(() => scope.resolve(key as never)).toThrow(UnknownDependencyLifetimeError);
+            }
+        );
     }
 );
