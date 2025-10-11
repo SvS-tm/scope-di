f3d3ed0af64544a2dc496585e91ec65a800870ca
SvS-NickM <svs.nickm@outlook.com>
2025-10-11 23:56:01 +0300

    chore: removed unused file

--- packages/system/src/helpers/constructor-parameters.ts
+++
@@ -1,6 +0,0 @@
-export type ConstructorParameters<T_Constructor extends new (...args: any[]) => any> = 
-(
-    T_Constructor extends new (...args: infer T_Args) => any 
-        ? T_Args 
-        : never
-);
